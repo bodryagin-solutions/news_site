95c2fb4e8761b4ee2463ebee91dc418335e854c4
Andrey Bodryagin <andrey.bodryagin@icloud.com>
2023-10-01 11:35:49 +0300

    add auth_app

--- app/tools/auth.py
+++ app/tools/auth.py
@@ -11,3 +11,6 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7", algorithm="HS256")
     return encoded_jwt
+
+
+#add auth_app
